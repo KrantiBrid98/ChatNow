9d83bca50b5544df937d1e432cde80eb68376395
KrantiBrid98 <45895279+KrantiBrid98@users.noreply.github.com>
2021-06-27 21:43:21 +0530

    added index

---
+++ index.js
@@ -0,0 +1,21 @@
+const express =  require('express');
+const http = require('http')
+const app = express();
+const server = http.createServer(app);
+const {Server} = require('socket.io')
+const io = new Server(server)
+
+app.get('/', (req, res) => res.sendFile(__dirname + '/index.html'))
+
+io.on('connection', (socket) => { // listen on the connection event for incoming sockets and log it to the console.
+    console.log("a user connected")
+    socket.on('chat message', (msg) => {
+        io.emit('chat message', msg); // to SEND an event to everyone, Socket.IO gives us the io.emit() method.
+      });
+    socket.on('disconnect', () => {
+        console.log('user disconnected');
+    })
+})
+server.listen(3000, ()=>{
+    console.log("server listening on port 3000")
+})
